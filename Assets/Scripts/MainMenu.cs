465ff916be9f8f262ff8c7af4e7662c39273a699
RyanGlem <ryanglem@hotmail.com>
2021-04-27 17:33:37 -0400

    added more levels and scene selection

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenu : MonoBehaviour
+{
+    public void ExitButton() {
+        Application.Quit();
+        Debug.Log ("Game closed");
+    }
+
+    public void BackButton() {
+        SceneManager.LoadScene ("MainMenu");
+    }
+
+    public void MathSelection() {
+        SceneManager.LoadScene ("SampleScene");
+    }
+
+    public void CapitalSelection() {
+        SceneManager.LoadScene ("Level 3");
+    }
+
+    public void ScienceSelection() {
+        SceneManager.LoadScene ("Level 6");
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene("SelectionScreen");
+    }
+}
